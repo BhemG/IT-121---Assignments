82ccafa52477bc5b4b97b570243c2589a9a9db69
Bethy Mhel Goyal <100412933+BhemG@users.noreply.github.com>
2022-04-01 21:01:41 +0800

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -39,9 +39,29 @@
       
       	
        	//////////////////////////////////////
-      	// ADD YOUR CODE HERE
-      	// FOLLOW THE EXAMPLE PROVIDED
-      	// FOR THE LAST BIT, THE 6th
+	       
+   public class BinaryToDecimalExample3{    
+public static int getDecimal(int binary){  
+    int decimal = 0;  
+    int n = 0;  
+    while(true){  
+      if(binary == 0){  
+        break;  
+      } else {  
+          int temp = binary%10;  
+          decimal += temp*Math.pow(2, n);  
+          binary = binary/10;  
+          n++;  
+       }  
+    }  
+    return decimal;  
+}  
+public static void main(String args[]){    
+System.out.println("Decimal of 1010 is: "+getDecimal(1010));  
+System.out.println("Decimal of 10101 is: "+getDecimal(10101));  
+System.out.println("Decimal of 11111 is: "+getDecimal(11111));  
+}}    
+	       
       	//////////////////////////////////////
 /*      				
          bit=binary%10;//get the last bit (6th)
